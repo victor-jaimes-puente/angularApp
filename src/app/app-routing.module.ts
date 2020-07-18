e7a4e3770b0209fc8a213f0d9273e5d934cb95ee
VictorJP <victor.jaimes.puente@gmail.com>
2020-07-17 23:32:42 -0500

    adding calculator compponent to angularJS for practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CalculatorComponent} from './calculator/calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calculator', component: CalculatorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
